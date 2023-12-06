ffc712d50b7e1725173aecefc8bfedcb5d2913b8
JustBeino <jbeinoras@yahoo.com>
2023-12-06 07:48:05 +0200

    bandom sukurti

---
+++ main.py
@@ -0,0 +1,32 @@
+
+"""
+Užduotis: kryžiukų nuliukų žaidimas (Tic Tac Toe)
+
+Sukurti kryžiukų/nuliukų žaidimą, kuris:
+Leistų žaisti dviems žaidėjams (X ir O)
+Teisingai fiksuotų vieno iš žaidėjų laimėjimą arba lygiasias ir stabdytų žaidimą
+Žaidimas vyktų konsolėje, grafinio interfeiso nereikia (bet galima daryti, tada konsolės nebereikia)
+Sukurtą žaidimą paskelbti github repozitorijoje,
+nuorodą paskelbti teamsuose, tam skirtoje užduotyje (Assignments)
+
+PAPILDOMAI (nebūtina):
+Kad baigus žaidimą, programa neišsijungtų, o leistų pakartoti žaidimą.
+Taip pat būtų galimybė išjungti programą.
+Kad žaidimas skaičiuotų abiejų žaidėjų sesijos laimėjimus.
+Leistų žaisti prieš kompiuterį (sukurti logiką, kaip jis elgsis)
+Padaryti GUI (su tkinter, pygame, dar kažkuo)
+"""
+def kryziukai(n):
+    x = (7, 8, 9)
+    y = (4, 5, 6)
+    z = (1, 2, 3)
+
+    #case
+    print(x)
+    print(y)
+    print(z)
+
+
+kryziukai(9)
+n = input()
+
