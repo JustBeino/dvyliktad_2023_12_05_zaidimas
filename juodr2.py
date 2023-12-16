cb981acd87b4cbc6b36cddd232a05e64c03650ea
JustBeino <jbeinoras@yahoo.com>
2023-12-16 04:06:29 +0200

    masyvo pic geriau

--- juodr2.py
+++ juodr2.py
@@ -3,7 +3,7 @@
 #
 import sys
 
-m = [[7,8,9],[4,5,6],[1,2,3]]
+m = [[7, 8, 9],[4, 5, 6],[1, 2, 3]]
 zaidejasx = "x"
 zaidejaso = "o"
 
@@ -18,8 +18,11 @@ sumos8eil = (m[2][0], m[1][1], m[0][2])     #istrizaine
 
 sumu_listas = (sumos1eil, sumos2eil, sumos3eil, sumos4eil, sumos5eil, sumos6eil, sumos7eil, sumos8eil)
 def masyvopic(m):
-    for y in m:
-        print(y)
+#    for y in m:        #padarysim graziau
+#        print(f" {y} ")
+    print(f"  {m[0][0]}  {m[0][1]}  {m[0][2]}")
+    print(f"  {m[1][0]}  {m[1][1]}  {m[1][2]}")
+    print(f"  {m[2][0]}  {m[2][1]}  {m[2][2]}")
 
 def tikrinam (z, a1, b1, zai):
     if z[a1][b1] != "x" and z[a1][b1] != "o":
@@ -31,6 +34,7 @@ def tikrinam (z, a1, b1, zai):
         #masyvopic(z)
         return False
 
+"""
 def tikrinam2 (masel, a, b, zai):
     if tikrinam(masel, a, b) == True:
         masel[a][b] = zai
@@ -38,11 +42,11 @@ def tikrinam2 (masel, a, b, zai):
         return (False, masel)
     else:
         return True
-
+"""
 
 def ivedimas(zaid):
     while True:
-        match input("Iveskite  1-9\n"):
+        match input("Iveskite  1-9, 0 lentele, + iseiti\n"):
             case "1":
                 print("ivedei 1")
                 if tikrinam(m, 2,0, zaid) == True:
@@ -60,40 +64,48 @@ def ivedimas(zaid):
                 #masyvopic(m)
             case "4":
                 print("ivedei 4")
-                m[1][0] = zaid
+                if tikrinam(m, 1, 0, zaid) == True:
+                    break
                 #masyvopic(m)
             case "5":
                 print("ivedei 5")
-                m[1][1] = zaid
+                if tikrinam(m, 1, 1, zaid) == True:
+                    break
                 #masyvopic(m)
             case "6":
                 print("ivedei 6")
-                m[1][2] = zaid
+                if tikrinam(m, 1, 2, zaid) == True:
+                    break
                 #masyvopic(m)
             case "7":
                 print("ivedei 7")
-                m[0][0] = zaid
+                if tikrinam(m, 0, 0, zaid) == True:
+                    break
                 #masyvopic(m)
             case "8":
                 print("ivedei 8")
-                m[0][1] = zaid
+                if tikrinam(m, 0, 1, zaid) == True:
+                    break
                 #masyvopic(m)
             case "9":
                 print("ivedei 9")
-                m[0][2] = zaid
+                if tikrinam(m, 0, 2, zaid) == True:
+                    break
                 #masyvopic(m)
-            case "\27":
+            case "+":
                 return exit()
                 #sys.exit()
+            case "0":
+                masyvopic(m)
             case _:
                 print("ne ta ivedei")
                 continue
 
 while True:
-    print("zaidejo X ejimas:")
+    print("------------------\nDabar zaidejo X ejimas")
     ivedimas(zaidejasx)
     masyvopic(m)
-    print("zaidejo O ejimas:")
+    print("------------------\nDabar zaidejo O ejimas")
     ivedimas(zaidejaso)
     masyvopic(m)
 
